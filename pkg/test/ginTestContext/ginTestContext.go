7401aa1717274bcc8ed7e00c52876a4264699335
adrichem <30172575+adrichem@users.noreply.github.com>
2022-12-30 14:57:00 +0100

    small cleanup of test code

--- pkg/test/ginTestContext/ginTestContext.go
+++ pkg/test/ginTestContext/ginTestContext.go
@@ -9,43 +9,6 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-type TestResponseRecorder struct {
-	*httptest.ResponseRecorder
-	closeChannel chan bool
-}
-
-func (r *TestResponseRecorder) CloseNotify() <-chan bool {
-	return r.closeChannel
-}
-
-func (r *TestResponseRecorder) closeClient() {
-	r.closeChannel <- true
-}
-
-func CreateTestResponseRecorder() *TestResponseRecorder {
-	return &TestResponseRecorder{
-		httptest.NewRecorder(),
-		make(chan bool, 1),
-	}
-}
-
-/*
-When testing the combination of gin and the http reverse proxy, we get panics like:
-panic: interface conversion: *httptest.ResponseRecorder is not http.CloseNotifier: missing method CloseNotify
-For this we need a have a custom recorder that implements httptest.ResponseRecorder and the CloseNotify method
-*/
-func TestContextWithGinResponseRecorder() (*gin.Context, *TestResponseRecorder) {
-	rr := CreateTestResponseRecorder()
-	// creates a test context and gin engine
-	ctx, _ := gin.CreateTestContext(rr)
-	ctx.Request = &http.Request{
-		Method: "GET",
-		Header: make(http.Header),
-		URL:    &url.URL{},
-	}
-	return ctx, rr
-}
-
 func TestContext() (*gin.Context, *httptest.ResponseRecorder) {
 	gin.SetMode(gin.TestMode)
 	responseWriter := httptest.NewRecorder()
